4ed166fafb8bbe7ab40a3f9f5217382c2aaad5d5
longgood <ray@longgood.com.tw>
2022-05-13 11:42:42 +0800

    新增自動檔

---
+++ _linux.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "----start----"
+cd /home/ray/CloudMajesty
+echo "------------"
+source env/bin/activate
+export FLASK_ENV=development
+export FLASK_DEBUG=True
+export FLASK_APP=run.py
+flask run --host="0.0.0.0" --port=80
+echo "-------------gogo-----------------"
+
