a4392607626d0489269e531eff53f2cb32ab8807
longgood <ray@longgood.com.tw>
2023-05-24 13:44:46 +0800

    update

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -4,26 +4,96 @@ Copyright (c) 2019 - present AppSeed.us
 """
 from apps import db
 from apps.home import blueprint
-from flask import render_template, request
+from flask import render_template, request,send_file
 from flask_login import login_required
 from jinja2 import TemplateNotFound
-
+import os
 
 @blueprint.route('/alter_db')
 def alter_db():
-    """
-    result=db.engine.execute("ALTER TABLE TProject ADD parentid Integer")
-    result=db.engine.execute("ALTER TABLE TProject ADD budge Integer")
-    result=db.engine.execute("ALTER TABLE TProject ADD winrate Integer")
-    result=db.engine.execute("ALTER TABLE TProject ADD priority Integer")
-    """
     result=db.engine.execute("ALTER TABLE TProject ADD startTime DATETIME ")
     result=db.engine.execute("ALTER TABLE TProject ADD endTime DATETIME ")
     return "updated"
 
 @blueprint.route('/webgl')
 def webgl():
+    return render_template('home/webglIndex.html', segment='index')
+@blueprint.route('/webglex')
+def webglex():
     return render_template('home/deploayWebGL/index.html', segment='index')
+
+image_directory = 'D://Dropbox//tmpp20230407//outputimage//20230415_155522_1_thread//'
+
+@blueprint.route('/getimage/<filename>')
+def get_image(filename):
+    image_path = os.path.join(image_directory, filename)
+    if os.path.exists(image_path):
+        return send_file(image_path, mimetype='image/jpeg')
+    else:
+        return "Image not found", 404
+@blueprint.route('/getcsv/<filename>')
+def get_csv(filename):
+    image_path = os.path.join(image_directory, filename)
+    print("ImagePath:",image_path)
+    if os.path.exists(image_path):
+        return send_file(image_path, mimetype='text/csv')
+    else:
+        return "CSV not found", 404
+#-----------------處理webgl的呼叫-------------------------------
+image_directory = 'D://Dropbox//tmpp20230407//outputimage//'
+
+@blueprint.route('/textfile',methods=['GET'])
+def send_textfile():
+    global image_directory
+    # Logic to retrieve the text file
+    userid=request.args.get('userid')
+    if userid == "001walk":
+        folder = image_directory+userid
+    else:
+        folder = image_directory+"000walk"
+    filename = folder+"//outputfile.csv"
+    print("send_textfile",filename)
+    return send_file(filename, mimetype="text/plain")
+    
+
+#http://localhost:5000/imagenamelist?userid=001walk
+@blueprint.route('/imagenamelist',methods=['GET'])
+def image_filename():
+    global image_directory
+    # Logic to retrieve the text file
+    userid=request.args.get('userid')
+    
+    print("USERID:",userid)
+    if userid == "001walk":
+            folder = 'D://Dropbox//tmpp20230407//outputimage//'+userid
+            result="001walk@"
+            print("image_filename:",result)
+    else:
+            folder = 'D://Dropbox//tmpp20230407//outputimage//'+"000walk"
+            result="000walk@"    
+            print("image_filename",result)
+    namelist=[_ for _ in os.listdir(folder) if _.endswith(".jpg")]
+    for name in namelist:
+        result=result+name.split(".")[0]+";"
+    return result
+#http://localhost:5000/getimages?userid=000walk&filename=0000000
+@blueprint.route('/getimages',methods=['GET'])
+def get_images():
+    userid=request.args.get('userid')
+    filename=request.args.get('filename')
+    file_path = os.path.join("D://Dropbox//tmpp20230407//outputimage//"+userid, filename+".jpg")
+    print("filepath",file_path,",exist:",os.path.exists(file_path))
+    return send_file(file_path, mimetype="image/jpeg")
+    
+    
+
+
+
+
+
+
+
+
 
 @blueprint.route('/index')
 @login_required
