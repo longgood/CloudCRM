5c60de2d44be0be7ecec0dd73c0788693f512c01
longgood <ray@longgood.com.tw>
2023-10-31 10:55:28 +0800

    update run
    可以改為對外模式

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 # -*- encoding: utf-8 -*-
 """
-Copyright (c) 2019 - present AppSeed.us
 """
 
 from flask_migrate import Migrate
@@ -11,24 +10,19 @@ from apps.config import config_dict
 from apps import create_app, db
 
 # WARNING: Don't run with debug turned on in production!
-print("before DEBUG config")
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 # The configuration
 get_config_mode = 'Debug' if DEBUG else 'Production'
 
-
-##--測試
-#get_config_mode ='Production'
-
-print("ray:get_config_mode:",get_config_mode.capitalize())
 try:
 
     # Load the configuration using the default values
     app_config = config_dict[get_config_mode.capitalize()]
-    
+
 except KeyError:
     exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
+
 app = create_app(app_config)
 Migrate(app, db)
 
@@ -36,8 +30,6 @@ if DEBUG:
     app.logger.info('DEBUG       = ' + str(DEBUG))
     app.logger.info('Environment = ' + get_config_mode)
     app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
+
 if __name__ == "__main__":
-   
-    app.debug=True
-    app.run(debug=True)
-    print("start run")
+    app.run(debug=True, host="0.0.0.0",port=80)
