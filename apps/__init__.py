51d7ad9861700a3896d4bbdac6b62e988219b731
longgood <ray@longgood.com.tw>
2025-12-20 14:59:43 +0800

    add namecard OCR feature and update requirements

--- apps/__init__.py
+++ apps/__init__.py
@@ -17,7 +17,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('authentication', 'home','reports','customer','webgames'):
+    for module_name in ('authentication', 'home','reports','customer','webgames','namecard'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
